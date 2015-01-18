483afcf0572389b671bbbdf930e062a6d4afbc38
Ruben Van den Bossche <rubenvdb@gmail.com>
2015-01-18 12:08:05 +0100

    Remove temp dir again; Reorder imports; Follow PEP8 style guides (except for col width, which is 99 instead of 79) Fix no output when not running in debug mode; Fix output format, don't show timestamps; Check S3 access and bucket in constructor

--- src/s3_site_cache_optimizer/optimize.py
+++ src/s3_site_cache_optimizer/optimize.py
@@ -12,15 +12,15 @@ import os.path
 import logging
 import fileinput
 import re
-from boto import connect_s3
-from boto.s3.key import Key
-from boto.exception import BotoClientError, BotoServerError
 from pkg_resources import Requirement, resource_filename, require
 from hashlib import sha256
-from shutil import copyfile, move
+from shutil import copyfile, move, rmtree
 from fnmatch import fnmatch
 from tempfile import mkdtemp, mkstemp
 from urlparse import urlparse
+from boto import connect_s3
+from boto.s3.key import Key
+from boto.exception import BotoClientError, BotoServerError
 
 __author__ = "Ruben Van den Bossche"
 __email__ = "ruben@appstrakt.com"
@@ -44,7 +44,7 @@ def calculate_fingerprint(fname):
             hasher.update(buf)
             buf = f.read(blocksize)
         filehash = hasher.hexdigest()
-        #logger.debug("Fingerprint of {0} is {1}".format(fname, filehash))
+        logger.debug("Fingerprint of {0} is {1}".format(fname, filehash))
         return filehash
 
 
@@ -58,74 +58,103 @@ def convert_filename(filename, filehash):
 
 
 class OptimizerError(Exception):
+
     '''
     Error thrown by Optimizer class.
     '''
 
     def __init__(self, msg):
         self.msg = msg
+
     def __str__(self):
         return str(self.msg)
 
 
 class Optimizer(object):
+
     '''
     Optimizer class: Optimize a static website for hosting in S3.
     '''
 
-    def __init__(self, source_dir, destination_bucket, exclude=[], output_dir=None, aws_access_key_id=None, aws_secret_access_key=None, skip_s3_upload=False):
+    def __init__(self, source_dir, destination_bucket, exclude=[], output_dir=None,
+                 aws_access_key_id=None, aws_secret_access_key=None, skip_s3_upload=False):
         '''
         Initialize Optimizer
         '''
 
-        logger.debug('Initialize Optimizer class')
+        logger.info('Initialize Optimizer class')
 
         if not os.path.isdir(source_dir):
             raise OptimizerError("{0} is not a valid path".format(source_dir))
 
         if not os.access(source_dir, os.R_OK):
-            raise OptimizerError("{0} is not a readable dir".format(source_dir))
+            raise OptimizerError(
+                "{0} is not a readable dir".format(source_dir))
 
-        if output_dir != None:
+        if output_dir is not None:
+            output_dir_is_temp = False
             if not os.path.isdir(output_dir):
                 try:
                     os.makedirs(output_dir)
                 except os.error as e:
-                    raise OptimizerError("{0} is not a directory and cannot be created".format(output_dir))
+                    raise OptimizerError(
+                        "{0} is not a directory and cannot be created".format(output_dir))
 
             if not os.access(output_dir, os.W_OK):
                 raise OptimizerError("{0} is not a writable dir".format(output_dir))
         else:
+            output_dir_is_temp = True
             output_dir = mkdtemp()
 
-        self._assets_ext = ['.css', '.svg', '.ttf', '.woff', '.woff2', '.otf', '.eot', '.png', '.jpg', '.jpeg', '.gif', '.js']
+        self._assets_ext = ['.css', '.svg', '.ttf', '.woff', '.woff2', '.otf', '.eot', '.png',
+                            '.jpg', '.jpeg', '.gif', '.js']
         self._rewriteables_ext = ['.html', '.htm', '.js', '.css']
 
         self._source_dir = source_dir
         self._output_dir = output_dir
+        self._output_dir_is_temp = output_dir_is_temp
         self._destination_bucket = destination_bucket
         self._subdirs = []
         self._files = []
         self._assets_map = {}
         self._rewritables = []
         self._exclude = exclude
-        self._aws_access_key_id = aws_access_key_id
-        self._aws_secret_access_key = aws_secret_access_key
         self._skip_s3_upload = skip_s3_upload
+
+        if not self._skip_s3_upload:
+            try:
+                self._s3 = connect_s3(aws_access_key_id=aws_access_key_id,
+                                      aws_secret_access_key=aws_secret_access_key)
+            except (BotoClientError, BotoServerError) as e:
+                raise OptimizerError("Cannot connect to S3")
+
+            try:
+                self._bucket = self._s3.get_bucket(self._destination_bucket)
+            except (BotoClientError, BotoServerError) as e:
+                raise OptimizerError("Bucket {0} does not exist or is not accessible."
+                                     .format(self._destination_bucket))
 
         logger.debug('Optimizer class initialized')
 
+    def __del__(self):
+        if self._output_dir_is_temp:
+            try:
+                logger.debug('Deleting temporary directory')
+                rmtree(self._output_dir)
+            except OSError as exc:
+                raise OptimizerError("Can't delete temporary directory.")
 
     def _index_source_dir(self):
         '''
         Index all files and directories under the source directory
         '''
 
-        logger.debug('Indexing source dir')
+        logger.info('Indexing source dir')
         for dirpath, dirnames, fnames in os.walk(self._source_dir):
 
             for d in dirnames:
-                relpath = os.path.relpath(os.path.join(dirpath, d), self._source_dir)
+                relpath = os.path.relpath(
+                    os.path.join(dirpath, d), self._source_dir)
                 if True in [fnmatch(relpath, exclude) for exclude in self._exclude]:
                     continue
 
@@ -133,7 +162,8 @@ class Optimizer(object):
                 self._subdirs.append(relpath)
 
             for f in fnames:
-                relpath = os.path.relpath(os.path.join(dirpath, f), self._source_dir)
+                relpath = os.path.relpath(
+                    os.path.join(dirpath, f), self._source_dir)
                 if True in [fnmatch(relpath, exclude) for exclude in self._exclude]:
                     continue
 
@@ -142,7 +172,8 @@ class Optimizer(object):
                 ext = os.path.splitext(f)[1]
                 if ext in self._assets_ext:
                     logger.debug("Found asset {0}".format(f))
-                    self._assets_map[relpath] = {'basename': os.path.basename(relpath)}
+                    self._assets_map[relpath] = {
+                        'basename': os.path.basename(relpath)}
 
                 if ext in self._rewriteables_ext:
                     logger.debug("Found rewritable {0}".format(f))
@@ -150,13 +181,12 @@ class Optimizer(object):
 
         logger.debug('Finished indexing source dir')
 
-
     def _calculate_fingerprints(self):
         '''
         Calculate fingerprints of all assets in the source dir
         '''
 
-        logger.debug('Calculating fingerprints')
+        logger.info('Calculating fingerprints')
 
         for fname in self._assets_map.keys():
             fingerprint = calculate_fingerprint(os.path.join(self._source_dir, fname))
@@ -164,13 +194,12 @@ class Optimizer(object):
 
         logger.debug('Finished calculating fingerprints')
 
-
     def _write_dirs(self):
         '''
         Write directory structure to output folder
         '''
 
-        logger.debug('Writing dirs')
+        logger.info('Writing dirs')
         for reldir in self._subdirs:
             absdir = os.path.join(self._output_dir, reldir)
             if not os.path.isdir(absdir):
@@ -178,7 +207,6 @@ class Optimizer(object):
                 os.makedirs(absdir)
 
         logger.debug('Finished writing dirs')
-
 
     def _rewrite_file(self, src, dst):
         '''
@@ -194,37 +222,40 @@ class Optimizer(object):
                     for asset in self._assets_map.keys():
                         if self._assets_map[asset]['basename'] in line:
                             url_chars = '''a-z0-9''' + re.escape('''-_.~!#$&*+,/:;=?@[]''')
-                            regex = r'''[''' + url_chars + ''']*''' + re.escape(self._assets_map[asset]['basename']) + '''[''' + url_chars + ''']*'''
-                            #results = re.search(regex, line, re.IGNORECASE)
+                            regex = r'''[''' + url_chars + ''']*''' + \
+                                    re.escape(self._assets_map[asset]['basename']) + \
+                                    '[' + url_chars + ']*'
 
                             it = re.finditer(regex, line, re.IGNORECASE)
                             for result in it:
                                 url = result.group()
-                                print (url)
                                 parsed_url = urlparse(url)
-                                if parsed_url.netloc != '' and parsed_url.netloc != self._destination_bucket:
-                                    # leave this url alone, is third party resource
-                                    logger.info("Skipping url {0}.".format(url))
+                                if parsed_url.netloc and \
+                                        parsed_url.netloc != self._destination_bucket:
+                                    # leave this url alone, is third party
+                                    logger.warning("Skipping rewriting url {0}.".format(url))
                                     continue
 
                                 logger.debug("Found asset {0} in {1}".format(url, src))
-                                normalized_relative_path = os.path.normpath(os.path.join(src_reldirpath, parsed_url.path)).lstrip('/')
+                                normalized_relative_path = os.path.normpath(
+                                    os.path.join(src_reldirpath, parsed_url.path)).lstrip('/')
 
                                 if asset == normalized_relative_path:
-                                    new_path = '/' + os.path.join(os.path.dirname(normalized_relative_path), os.path.basename(self._assets_map[asset]['new_filename']))
+                                    new_path = '/' + os.path.join(
+                                        os.path.dirname(normalized_relative_path),
+                                        os.path.basename(self._assets_map[asset]['new_filename']))
                                     logger.debug("Replacing with {0}".format(new_path))
 
                                     line = line[:result.start()] + new_path + line[result.end():]
 
                     f_dst.write(line)
 
-
     def _write_files(self):
         '''
         Write files to output folder, and rewrite file names/content if necessary.
         '''
 
-        logger.debug('Writing files')
+        logger.info('Writing files')
         assets = set(self._assets_map.keys())
         rewritables = set(self._rewritables)
 
@@ -241,13 +272,15 @@ class Optimizer(object):
         # (2) Assets that are also rewritables
         for src_filename in assets & rewritables:
             # we assume these files don't have mutual references to eachother
-            # if they do, we need to build a dependency tree and solve order that way (TODO)
+            # if they do, we need to build a dependency tree and solve order
+            # that way (TODO)
 
             # make temp file
             tmp_handle, tmp_filename = mkstemp(text=True)
 
             src = os.path.join(self._source_dir, src_filename)
-            logger.debug("2. Rewriting asset {0} to temp file".format(src_filename))
+            logger.debug(
+                "2. Rewriting asset {0} to temp file".format(src_filename))
             self._rewrite_file(src, tmp_filename)
 
             # close temp file and calculate fingerprint
@@ -279,18 +312,14 @@ class Optimizer(object):
 
         logger.debug('Finished writing files')
 
-
     def _upload_to_bucket(self):
         '''
         Upload contents of output folder to S3.
         '''
-        logger.debug('Uploading to bucket')
+        logger.info('Uploading to bucket')
 
         try:
-            s3 = connect_s3(aws_access_key_id=self._aws_access_key_id, aws_secret_access_key=self._aws_secret_access_key)
-            bucket = s3.get_bucket(self._destination_bucket)
-
-            to_be_deleted = [l.key for l in bucket.list()]
+            to_be_deleted = [l.key for l in self._bucket.list()]
 
             for dirpath, dirnames, fnames in os.walk(self._output_dir):
 
@@ -307,12 +336,12 @@ class Optimizer(object):
                     # check if file should be cached / reuploaded
                     is_asset = os.path.splitext(f)[1] in self._assets_ext
 
+                    k = self._bucket.get_key(relpath)
+                    if k is None or not is_asset:
+                        # asset doesn't exist or file is not an asset and
+                        # should be uploaded anyway
 
-                    k = bucket.get_key(relpath)
-                    if k == None or not is_asset:
-                        # asset doesn't exist or file is not an asset and should be uploaded anyway
-
-                        k = Key(bucket)
+                        k = Key(self._bucket)
                         k.key = relpath
 
                         headers = {}
@@ -323,19 +352,20 @@ class Optimizer(object):
                             # set no-cache headers
                             headers['Cache-Control'] = "no-cache, max-age=0"
 
-                        logger.debug("Uploading file {0} to {1}".format(relpath, self._destination_bucket))
+                        logger.debug(
+                            "Uploading file {0} to {1}".format(relpath, self._destination_bucket))
                         k.set_contents_from_filename(abspath, replace=True, headers=headers)
 
             # remove files not currently touched
             for del_file in to_be_deleted:
-                logger.debug("Deleting key {0} from {1}".format(del_file, self._destination_bucket))
-            bucket.delete_keys(to_be_deleted)
+                logger.debug(
+                    "Deleting key {0} from {1}".format(del_file, self._destination_bucket))
+            self._bucket.delete_keys(to_be_deleted)
 
         except (BotoClientError, BotoServerError) as e:
             raise OptimizerError("Error uploading to S3" + str(e))
 
         logger.debug('Finished uploading to bucket')
-
 
     def run(self):
         '''
@@ -350,7 +380,7 @@ class Optimizer(object):
         if not self._skip_s3_upload:
             self._upload_to_bucket()
 
-        logger.debug('Finished running optimize')
+        logger.info('Finished optimizing static website for S3.')
 
 
 def main():
@@ -362,21 +392,34 @@ def main():
     global logger
     logger = logging.getLogger('s3-site-cache-optimizer')
     ch = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
     # parse arguments
-    parser = argparse.ArgumentParser(prog='s3-site-cache-optimizer', description='Optimize a static website for hosting in S3, by including a fingerprint into all assets\' filenames. The optimized website is uploaded into the specified S3 bucket with the right cache headers.')
+    parser = argparse.ArgumentParser(
+        prog='s3-site-cache-optimizer',
+        description='Optimize a static website for hosting in S3, by including a fingerprint \
+        into all assets\' filenames. The optimized website is uploaded into the specified S3 \
+        bucket with the right cache headers.')
     parser.add_argument('--debug', action='store_true', help='Enable debug output')
-    parser.add_argument('--version', action='version', version='%(prog)s ' + str(require("s3-site-cache-optimizer")[0].version))
+    parser.add_argument('--version', action='version', version='%(prog)s ' +
+                        str(require("s3-site-cache-optimizer")[0].version))
     parser.add_argument("source_dir", help='Local directory containing a static website.')
     parser.add_argument("destination_bucket", help='Domain name of the website and S3 bucket name.')
-    parser.add_argument('--exclude', nargs='+', metavar="PATTERN", default=[], help='Exclude files and directories matching these patterns.')
-    parser.add_argument('-o', '--output', dest='output_dir', default=None, help='Output directory in which local files are written. When absent a temporary directory is created and used.')
-    parser.add_argument('--access-key', dest="aws_access_key_id", default=None, help='AWS access key. If this field is not specified, credentials from environment or credentials files will be used.')
-    parser.add_argument('--secret-key', dest="aws_secret_access_key", default=None, help='AWS access secret. If this field is not specified, credentials from environment or credentials files will be used.')
-    parser.add_argument('--skip-s3-upload', dest="skip_s3_upload", action='store_true', help='Skip uploading to S3.')
+    parser.add_argument('--exclude', nargs='+', metavar="PATTERN", default=[],
+                        help='Exclude files and directories matching these patterns.')
+    parser.add_argument('-o', '--output', dest='output_dir', default=None,
+                        help='Output directory in which local files are written. When absent a \
+                        temporary directory is created and used.')
+    parser.add_argument('--access-key', dest="aws_access_key_id", default=None,
+                        help='AWS access key. If this field is not specified, credentials from \
+                        environment or credentials files will be used.')
+    parser.add_argument('--secret-key', dest="aws_secret_access_key", default=None,
+                        help='AWS access secret. If this field is not specified, credentials from \
+                        environment or credentials files will be used.')
+    parser.add_argument('--skip-s3-upload', dest="skip_s3_upload",
+                        action='store_true', help='Skip uploading to S3.')
 
     try:
         args = parser.parse_args()
@@ -387,9 +430,14 @@ def main():
     # set logging level
     if args.debug:
         logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
 
     try:
-        Optimizer(args.source_dir, args.destination_bucket, exclude=args.exclude, output_dir=args.output_dir, aws_access_key_id=args.aws_access_key_id, aws_secret_access_key=args.aws_secret_access_key, skip_s3_upload=args.skip_s3_upload).run()
+        Optimizer(args.source_dir, args.destination_bucket, exclude=args.exclude,
+                  output_dir=args.output_dir, aws_access_key_id=args.aws_access_key_id,
+                  aws_secret_access_key=args.aws_secret_access_key,
+                  skip_s3_upload=args.skip_s3_upload).run()
     except Exception as e:
-        logger.error(e)
+        logger.critical(e)
         exit(1)
