eb52ef387ccb58f88fa6fd5d0bb034758e068e89
Matteo De Wint <matteo@novemberfive.co>
2019-09-03 10:18:06 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='s3-site-cache-optimizer',
-    version='0.8.0',
+    version='0.8.1',
     license='MIT',
 
     description='Optimize a static website before uploading to S3',
